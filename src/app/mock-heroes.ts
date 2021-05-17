f20748ec871227e546e90210dba98d93e5c3d621
AdrianPerez550 <201822143@tese.edu.mx>
2021-05-17 00:36:07 -0500

    2 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,8 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {  id: 11, name: 'Hulk',age: 50, power:'Superfuerza',universe:'Marvel', secret_identity: "Bruce Banner"},
+  {  id: 11, name: 'Hulk',age: 50, power:'Superfuerza',
+  universe:'Marvel', secret_identity: "Bruce Banner"},
 
   {id: 12, name: 'Superman', age:40,power:'Superfuerza y Supervuelo',
   universe:'DC Comics', secret_identity: "Clack Joseph Kent"  },
@@ -15,19 +16,23 @@ export const HEROES: Hero[] = [
   {id: 15, name: 'Iron Man', age:56,power:'Inteligencia sobrehumana',
   universe:'Marvel', secret_identity: "Tony stark"},
 
-  {id: 16, name: 'Wonder Woman', age:45,power:'Superfuerza y Super Vuelo',universe:'DC Comics', secret_identity: "Diana de Temiscira" },
+  {id: 16, name: 'Wonder Woman', age:45,power:'Superfuerza y Super Vuelo',
+  universe:'DC Comics', secret_identity: "Diana de Temiscira" },
 
-  {id: 17, name: 'Robin', age:25,power:'Superfuerza',universe:'DC Comics', secret_identity: "Damian Wayne" },
+  {id: 17, name: 'Robin', age:25,power:'Superfuerza',
+  universe:'DC Comics', secret_identity: "Damian Wayne" },
 
    {id: 17, name: 'Bruja escarlata', age:35,power:'Proyeccion astral',
   universe:'Marvel', secret_identity: "Wanda Maximoff"},
 
-  {id: 18, name: 'Capitana Marvel', age:32,power:'Superfuerza y Supervuelo',universe:'Marvel', secret_identity: "Carol Danvers"},
+  {id: 18, name: 'Capitana Marvel', age:32,power:'Superfuerza y Supervuelo',
+  universe:'Marvel', secret_identity: "Carol Danvers"},
 
-  {id: 19, name: 'Cibord', age:28,power:'Superobot',universe:'DC Comics', secret_identity: "Victor Stone"},
+  {id: 19, name: 'Cibord', age:28,power:'Superobot',
+  universe:'DC Comics', secret_identity: "Victor Stone"},
 
-
-  {id: 20, name: 'Ant-Man', age:38 ,power:'Cambiar de tamaño',universe:'Marvel', secret_identity: "Scott Lang"}
+  {id: 20, name: 'Ant-Man', age:38 ,power:'Cambiar de tamaño',
+  universe:'Marvel', secret_identity: "Scott Lang"}
 
   
 ];
