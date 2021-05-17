4970f75e60fe87b99972aca2b02cf0effbfa125f
AdrianPerez550 <201822143@tese.edu.mx>
2021-05-17 00:41:52 -0500

    2 files modified

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,25 +8,25 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Hulk',age: 50,power:'Superfuerza',
+      { id: 10, name: 'Hulk',age: 50,power:'Superfuerza',
       universe:'Marvel',secret_identity: "Bruce Banner" },
       
-      { id: 12,  name: 'Superman', age:40,power:'Superfuerza',
+      { id: 11,  name: 'Superman', age:40,power:'Superfuerza',
       universe:'DC Comics', secret_identity: "Clack Joseph Kent" },
 
-      { id: 13, name: 'Dr.Strange',age:55,power:'Supermagia',
+      { id: 12, name: 'Dr.Strange',age:55,power:'Supermagia',
        universe:'Marvel', secret_identity: "Perter Hooten"  },
 
-      { id: 14, name: 'Literna Verde', age:38,power:'Anillo de poder',
+      { id: 13, name: 'Literna Verde', age:38,power:'Anillo de poder',
        universe:'DC Comics', secret_identity: "Alan Scott" },
       
-       {id: 15, name: 'Iron Man', age:56,power:'Inteligencia sobrehumana',
+       {id: 14, name: 'Iron Man', age:56,power:'Inteligencia sobrehumana',
       universe:'Marvel', secret_identity: "Tony stark"},
 
-      {id: 16, name: 'Wonder Woman', age:45,power:'Superfuerza y Super Vuelo',
+      {id: 15, name: 'Wonder Woman', age:45,power:'Superfuerza y Super Vuelo',
       universe:'DC Comics', secret_identity: "Diana de Temiscira" },
 
-      {id: 17, name: 'Robin', age:25,power:'Superfuerza',
+      {id: 16, name: 'Robin', age:25,power:'Superfuerza',
       universe:'DC Comics', secret_identity: "Damian Wayne" },
 
       {id: 17, name: 'Bruja escarlata', age:35,power:'Proyeccion astral',
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,25 +1,25 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {  id: 11, name: 'Hulk',age: 50, power:'Superfuerza',
+  { id: 10, name: 'Hulk',age: 50, power:'Superfuerza',
   universe:'Marvel', secret_identity: "Bruce Banner"},
 
-  {id: 12, name: 'Superman', age:40,power:'Superfuerza y Supervuelo',
+  {id: 11, name: 'Superman', age:40,power:'Superfuerza y Supervuelo',
   universe:'DC Comics', secret_identity: "Clack Joseph Kent"  },
 
-  {id: 13, name: 'Dr.Strange', age:55,power:'Supermagia',
+  {id: 12, name: 'Dr.Strange', age:55,power:'Supermagia',
   universe:'Marvel', secret_identity: "Perter Hooten"  },
 
-  {id: 14, name: 'Literna Verde', age:38,power:'Anillo de poder',
+  {id: 13, name: 'Literna Verde', age:38,power:'Anillo de poder',
   universe:'DC Comics', secret_identity: "Alan Scott"  },
 
-  {id: 15, name: 'Iron Man', age:56,power:'Inteligencia sobrehumana',
+  {id: 14, name: 'Iron Man', age:56,power:'Inteligencia sobrehumana',
   universe:'Marvel', secret_identity: "Tony stark"},
 
-  {id: 16, name: 'Wonder Woman', age:45,power:'Superfuerza y Super Vuelo',
+  {id: 15, name: 'Wonder Woman', age:45,power:'Superfuerza y Super Vuelo',
   universe:'DC Comics', secret_identity: "Diana de Temiscira" },
 
-  {id: 17, name: 'Robin', age:25,power:'Superfuerza',
+  {id: 16, name: 'Robin', age:25,power:'Superfuerza',
   universe:'DC Comics', secret_identity: "Damian Wayne" },
 
   {id: 17, name: 'Bruja escarlata', age:35,power:'Proyeccion astral',
